51c27fc9f7bd3d3156c3e061cacd3e675bfdba3a
mathiasriedel <mathiasriedel12@gmai.com>
2022-10-14 17:54:11 +0300

    refactor initialization

--- fdibamon/src/main/java/com/example/App.java
+++ fdibamon/src/main/java/com/example/App.java
@@ -7,18 +7,7 @@ import java.util.Scanner;
 public class App {
     public static void main( String[] args )
     {
-        Fdibamon jan = new Fdibamon("Jan", 50, 60);
-        Fdibamon rado = new Fdibamon("Rado", 20, 30);
-        Fdibamon klaus = new Fdibamon("Klaus", 10, 3);
-        Fdibamon mathias = new Fdibamon("Mathias", 30, 50);
-        Fdibamon nikolay = new Fdibamon("Nikolay", 5, 15);
-        
-        List<Fdibamon> fdibamons = new ArrayList<Fdibamon>();
-        fdibamons.add(jan);
-        fdibamons.add(rado);
-        fdibamons.add(klaus);
-        fdibamons.add(mathias);
-        fdibamons.add(nikolay);
+        List<Fdibamon> fdibamons = initializeFdibamons();
 
         System.out.println("Folgende Fdibamons stehen zur Auswahl: Jan, Rado, Klaus, Mathias, Nikolay");
 
@@ -56,5 +45,23 @@ public class App {
             }
         }
         return fdibamon;
+    }
+
+    public static List<Fdibamon> initializeFdibamons() {
+        List<Fdibamon> fdibamons = new ArrayList<Fdibamon>();
+
+        Fdibamon jan = new Fdibamon("Jan", 50, 60);
+        Fdibamon rado = new Fdibamon("Rado", 20, 30);
+        Fdibamon klaus = new Fdibamon("Klaus", 10, 3);
+        Fdibamon mathias = new Fdibamon("Mathias", 30, 50);
+        Fdibamon nikolay = new Fdibamon("Nikolay", 5, 15);
+        
+        fdibamons.add(jan);
+        fdibamons.add(rado);
+        fdibamons.add(klaus);
+        fdibamons.add(mathias);
+        fdibamons.add(nikolay);
+
+        return fdibamons;
     }
 }
