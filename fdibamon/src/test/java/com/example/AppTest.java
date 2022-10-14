6288650c115607ae047b63920c56b160cdd46863
mathiasriedel <mathiasriedel12@gmai.com>
2022-10-14 19:49:02 +0300

    refactor unit tests

--- fdibamon/src/test/java/com/example/AppTest.java
+++ fdibamon/src/test/java/com/example/AppTest.java
@@ -10,61 +10,47 @@ import org.junit.Test;
 
 public class AppTest 
 {
-    @Test
-    public void fdibamonsShouldBeEqual() {
+    public static List<Fdibamon> initializeFdibamonsForTesting() {
+        List<Fdibamon> fdibamons = new ArrayList<Fdibamon>();
+        
         Fdibamon jan = new Fdibamon("Jan", 50, 60);
         Fdibamon rado = new Fdibamon("Rado", 20, 30);
         Fdibamon klaus = new Fdibamon("Klaus", 10, 3);
         Fdibamon mathias = new Fdibamon("Mathias", 30, 50);
         Fdibamon nikolay = new Fdibamon("Nikolay", 5, 15);
         
-        List<Fdibamon> fdibamons = new ArrayList<Fdibamon>();
         fdibamons.add(jan);
         fdibamons.add(rado);
         fdibamons.add(klaus);
         fdibamons.add(mathias);
         fdibamons.add(nikolay);
-        
-        Fdibamon fdibamon = App.getFdibamonFromList(fdibamons, "Rado");
-        assertEqual(fdibamon, rado);
+
+        return fdibamons;
     }
 
-    private void assertEqual(Fdibamon fdibamon, Fdibamon rado) {
+    @Test
+    public void fdibamonsShouldBeEqual() {
+        List<Fdibamon> fdibamons = initializeFdibamonsForTesting();
+
+        Fdibamon rado = fdibamons.get(1);
+        
+        Fdibamon fdibamon = App.getFdibamonFromList(fdibamons, "Rado");
+        assertEquals(fdibamon, rado);
     }
 
     @Test
     public void fdibamonsShouldNotBeEqual() {
-        Fdibamon jan = new Fdibamon("Jan", 50, 60);
-        Fdibamon rado = new Fdibamon("Rado", 20, 30);
-        Fdibamon klaus = new Fdibamon("Klaus", 10, 3);
-        Fdibamon mathias = new Fdibamon("Mathias", 30, 50);
-        Fdibamon nikolay = new Fdibamon("Nikolay", 5, 15);
-        
-        List<Fdibamon> fdibamons = new ArrayList<Fdibamon>();
-        fdibamons.add(jan);
-        fdibamons.add(rado);
-        fdibamons.add(klaus);
-        fdibamons.add(mathias);
-        fdibamons.add(nikolay);
+        List<Fdibamon> fdibamons = initializeFdibamonsForTesting();
+
+        Fdibamon klaus = fdibamons.get(2);
         
         Fdibamon fdibamon = App.getFdibamonFromList(fdibamons, "Rado");
         assertNotEquals(fdibamon, klaus);
     }
 
     @Test
-    public void fdibamonsShouldBeNull() {
-        Fdibamon jan = new Fdibamon("Jan", 50, 60);
-        Fdibamon rado = new Fdibamon("Rado", 20, 30);
-        Fdibamon klaus = new Fdibamon("Klaus", 10, 3);
-        Fdibamon mathias = new Fdibamon("Mathias", 30, 50);
-        Fdibamon nikolay = new Fdibamon("Nikolay", 5, 15);
-        
-        List<Fdibamon> fdibamons = new ArrayList<Fdibamon>();
-        fdibamons.add(jan);
-        fdibamons.add(rado);
-        fdibamons.add(klaus);
-        fdibamons.add(mathias);
-        fdibamons.add(nikolay);
+    public void fdibamonShouldBeNull() {
+        List<Fdibamon> fdibamons = initializeFdibamonsForTesting();
         
         Fdibamon fdibamon = App.getFdibamonFromList(fdibamons, "NotAName");
         assertEquals(fdibamon, null);
